e001a8308a7974cfdea101ce33e204531dd24bcc
ssangwon123 <sangwonjin370@gmail.com>
2026-06-06 21:46:41 +0900

    주석추가

--- seat_reservation_system/cli.py
+++ seat_reservation_system/cli.py
@@ -6,6 +6,7 @@ list                      - List all seats
 reserve <seat_id> <name>  - Reserve a seat
 cancel <seat_id> [name]   - Cancel a reservation
 status <seat_id>          - Show seat status
+# 예약자 이름 검색 명령어
 find <name>               - Find reservations by reserver name
 stats                     - Show summary stats
 help                      - Show this help
@@ -49,9 +50,10 @@ def run_cli():
                 _require_args(command, args, 1)
                 seat_id, name = store.status(int(args[0]))
                 _print_seat(seat_id, name)
+            
             elif command == "find":
                 _require_args(command, args, 1)
-
+                # 사용자가 입력한 검색어 조합
                 keyword = " ".join(args)
                 reservations = store.find_reservations_by_name(keyword)
 
